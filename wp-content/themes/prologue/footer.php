37c46718bfb8be3e54c38590d2de89c59bb5c774
David de Ugarte <david@bocadeoro.(none)>
2008-05-30 19:06:27 +0200

    añadido enlace en el footer al gzip y a github

--- wp-content/themes/prologue/footer.php
+++ wp-content/themes/prologue/footer.php
@@ -1,6 +1,9 @@
 <?php get_sidebar( ); ?>
 
 <div id="footer">
+
+Puedes descargar la <a href="http://exploradoreselectronicos.net/pressmark.tar.gz">última versión de Pressmark en formato gzip</a> o <a href="http://github.com/exploradoreselectronicos/pressmark/tree/master">hacer un <em>fork</em> desde github</a> para abrir tu propia línea de desarrollo 
+
 </div>
 
 </div> <!-- // wrapper -->
