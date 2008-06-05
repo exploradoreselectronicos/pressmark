4090ac802e2ecdc2237a731be652fb8e33360cb6
David de Ugarte <david@bocadeoro.(none)>
2008-06-05 21:53:45 +0200

    user cloud

--- wp-content/themes/prologue/functions.php
+++ wp-content/themes/prologue/functions.php
@@ -208,7 +208,7 @@ function user_cloud() {
 	
 	foreach ( (array) $authors as $author ) {
 		$author = get_userdata( $author->ID );
-		$level = get_userdata ( $author->userlevel );
+		$level = get_userdata ( $author->user_level );
 		if ($level==10)
 		{
 		echo '<a href="' . get_author_posts_url($author->ID, $author->user_nicename) . '" title="' . sprintf(__("Posts by %s"), attribute_escape($author->display_name)) . '">';
